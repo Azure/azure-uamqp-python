d25eb3e1585af6ff8c4342c6aa04d0712626b9e7
annatisch <antisch@microsoft.com>
2018-05-02 08:55:30 -0700

    Added utils docs

--- uamqp/utils.py
+++ uamqp/utils.py
@@ -14,6 +14,15 @@ from uamqp import c_uamqp
 
 
 def parse_connection_string(connect_str):
+    """Parse a connection string such as those provided by the Azure portal.
+    Connection string should be formatted like:
+     Key=Value;Key=Value;Key=Value
+    The connection string will be parsed into a dictionary.
+
+    :param connect_str: The connection string.
+    :type connect_str: str
+    :returns: dict[str, str]
+    """
     connect_info = {}
     fields = connect_str.split(';')
     for field in fields:
@@ -23,12 +32,40 @@ def parse_connection_string(connect_str):
 
 
 def create_sas_token(key_name, shared_access_key, scope, expiry=timedelta(hours=1)):
+    """Create a SAS token.
+
+    :param key_name: The username/key name/policy name for the token.
+    :type key_name: bytes
+    :param shared_access_key: The shared access key to generate the token from.
+    :type shared_access_key: bytes
+    :param scope: The token permissions scope.
+    :type scope: bytes
+    :param expiry: The lifetime of the generated token. Default is 1 hour.
+    :type expiry: ~datetime.timedelta
+    :returns: bytes
+    """
     shared_access_key = base64.b64encode(shared_access_key)
     abs_expiry = int(time.time()) + expiry.seconds
     return c_uamqp.create_sas_token(shared_access_key, scope, key_name, abs_expiry)
 
 
 def data_factory(value):
+    """Wrap a Python type in the equivalent C AMQP type.
+    If the Python type has already been wrapped in a ~uamqp.types.AMQPType
+    object - then this will be used to select the appropriate C type.
+    - bool => c_uamqp.BoolValue
+    - int => c_uamqp.IntValue
+    - str => c_uamqp.StringValue
+    - bytes => c_uamqp.BinaryValue
+    - str (char) => c_uamqp.CharValue
+    - uuid.UUID => c_uamqp.UUIDValue
+    - list/set/tuple => c_uamqp.ListValue
+    - dict => c_uamqp.DictValue (AMQP map)
+    - float => c_uamqp.DoubleValue
+
+    :param value: The value to wrap.
+    :returns: c_uamqp.AMQPValue
+    """
     result = None
     if value is None:
         result = c_uamqp.null_value()
