4aaa18a380b8f7c667bbc881d1148f8789626eb0
antisch <antisch@microsoft.com>
2018-06-18 14:47:42 +1200

    Updated uamqp-c to version 1.2.5

--- src/vendor/azure-uamqp-c/src/session.c
+++ src/vendor/azure-uamqp-c/src/session.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include "azure_c_shared_utility/optimize_size.h"
 #include "azure_c_shared_utility/gballoc.h"
-#include "azure_c_shared_utility/xlogging.h"
 #include "azure_uamqp_c/session.h"
 #include "azure_uamqp_c/connection.h"
 #include "azure_uamqp_c/amqp_definitions.h"
@@ -448,19 +447,20 @@ static void on_frame_received(void* context, AMQP_VALUE performative, uint32_t p
             {
                 end_session_with_error(session_instance, "amqp:decode-error", "Cannot get link role from ATTACH frame");
             }
-            else 
+            else
             {
+                LINK_ENDPOINT_INSTANCE* link_endpoint;
+
                 if (attach_get_source(attach_handle, &source) != 0)
                 {
                     source = NULL;
-                    //end_session_with_error(session_instance, "amqp:decode-error", "Cannot get link source from ATTACH frame");
                 }
-                else if (attach_get_target(attach_handle, &target) != 0)
+                if (attach_get_target(attach_handle, &target) != 0)
                 {
                     target = NULL;
-                    //end_session_with_error(session_instance, "amqp:decode-error", "Cannot get link target from ATTACH frame");
                 }
-                LINK_ENDPOINT_INSTANCE* link_endpoint = find_link_endpoint_by_name(session_instance, name);
+
+                link_endpoint = find_link_endpoint_by_name(session_instance, name);
                 if (link_endpoint == NULL)
                 {
                     /* new link attach */
@@ -544,6 +544,7 @@ static void on_frame_received(void* context, AMQP_VALUE performative, uint32_t p
                 {
                     if (link_endpoint->link_endpoint_state != LINK_ENDPOINT_STATE_DETACHING)
                     {
+                        link_endpoint->link_endpoint_state = LINK_ENDPOINT_STATE_DETACHING;
                         link_endpoint->frame_received_callback(link_endpoint->callback_context, performative, payload_size, payload_bytes);
                     }
                     else
