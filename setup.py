b86c12e55b97b67d4691d6be25136dddaf2b78be
Max Krivich <maxkrivich@gmail.com>
2018-10-14 11:43:37 +0300

    Add restriction on python version This argument described in doc https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires

--- setup.py
+++ setup.py
@@ -246,11 +246,12 @@ setup(
     zip_safe=False,
     include_package_data=True,
     packages=find_packages(exclude=["tests"]),
-    ext_modules = extensions,
+    ext_modules=extensions,
     install_requires=[
         "certifi>=2017.4.17",
     ],
     cmdclass={
         'build_ext': build_ext,
-    }    
+    },
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
 )
