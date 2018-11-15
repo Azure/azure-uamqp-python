f43ea5313b45498ed806b9d98cbc53467e364692
annatisch <antisch@microsoft.com>
2018-11-15 08:49:55 -0800

    Updated openssl OSX build script

--- build_openssl_osx.sh
+++ build_openssl_osx.sh
@@ -4,22 +4,21 @@
 
 curl -O https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
 tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
-mv openssl-$OPENSSL_VERSION openssl_i386_src
+mv openssl-$OPENSSL_VERSION openssl_i386
 tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
-mv openssl-$OPENSSL_VERSION openssl_x86_64_src
+mv openssl-$OPENSSL_VERSION openssl_x86_64
 tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
-mv openssl-$OPENSSL_VERSION openssl
-cd openssl_i386_src
-./Configure darwin-i386-cc shared --openssldir=/private/etc/ssl --prefix=$AGENT_TEMPDIRECTORY/openssl_i386
+mv openssl-$OPENSSL_VERSION $AGENT_TEMPDIRECTORY/openssl
+cd openssl_i386
+./Configure darwin-i386-cc shared --openssldir=/private/etc/ssl
 make
-make install
 cd ../
-cd openssl_x86_64_src
-./Configure darwin64-x86_64-cc shared --openssldir=/private/etc/ssl --prefix=$AGENT_TEMPDIRECTORY/openssl_x86_64
+cd openssl_x86_64
+./Configure darwin64-x86_64-cc shared --openssldir=/private/etc/ssl
 make
-make install
 cd ../
-cp -a $AGENT_TEMPDIRECTORY/openssl_x86_64/. $AGENT_TEMPDIRECTORY/openssl/
-lipo -create $AGENT_TEMPDIRECTORY/openssl_i386/lib/libcrypto.a $AGENT_TEMPDIRECTORY/openssl_x86_64/lib/libcrypto.a -output $AGENT_TEMPDIRECTORY/openssl/lib/libcrypto.a
-lipo -create $AGENT_TEMPDIRECTORYopenssl_i386/lib/libssl.a $AGENT_TEMPDIRECTORY/openssl_x86_64/lib/libssl.a -output $AGENT_TEMPDIRECTORY/openssl/lib/libssl.a
+cp -a ./openssl_x86_64/. $AGENT_TEMPDIRECTORY/openssl/
+mkdir $AGENT_TEMPDIRECTORY/openssl/lib
+lipo -create openssl_i386/libcrypto.a openssl_x86_64/libcrypto.a -output $AGENT_TEMPDIRECTORY/openssl/lib/libcrypto.a
+lipo -create openssl_i386/libssl.a openssl_x86_64/libssl.a -output $AGENT_TEMPDIRECTORY/openssl/lib/libssl.a
 rm openssl-$OPENSSL_VERSION.tar.gz
