aed45fc855d0d387ab06790b99208ea1c74424b3
Brandon Siegel <brandon.siegel@microsoft.com>
2019-02-19 16:39:52 -0800

    Refactor build matrix, only run windows builds for now

--- install_python_osx.sh
+++ install_python_osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -O https://www.python.org/ftp/python/$PYTHON_VERSION/python-$PYTHON_VERSION-macosx10.6.pkg
-sudo installer -allowUntrusted -pkg python-$PYTHON_VERSION-macosx10.6.pkg -target /
-rm -f python-$PYTHON_VERSION-macosx10.6.pkg
+curl -O https://www.python.org/ftp/python/$PYTHONVERSION/python-$PYTHONVERSION-macosx10.6.pkg
+sudo installer -allowUntrusted -pkg python-$PYTHONVERSION-macosx10.6.pkg -target /
+rm -f python-$PYTHONVERSION-macosx10.6.pkg
