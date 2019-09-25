da3b5d1ba07d847315d1672bf51ce08fd0950b3f
antisch <antisch@microsoft.com>
2019-09-25 10:06:42 -0700

    More logging

--- src/vendor/azure-uamqp-c/deps/azure-c-shared-utility/adapters/socketio_win32.c
+++ src/vendor/azure-uamqp-c/deps/azure-c-shared-utility/adapters/socketio_win32.c
@@ -515,6 +515,7 @@ void socketio_dowork(CONCRETE_IO_HANDLE socket_io)
                     received = recv(socket_io_instance->socket, (char*)socket_io_instance->recv_bytes, RECEIVE_BYTES_VALUE, 0);
                     if ((received > 0))
                     {
+                        LogError("Received bytes");
                         if (socket_io_instance->on_bytes_received != NULL)
                         {
                             /* Explicitly ignoring here the result of the callback */
