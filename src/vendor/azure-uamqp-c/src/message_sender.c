4a95591c37b7165751061e6142e3bda87b8cf734
antisch <antisch@microsoft.com>
2020-01-13 10:23:01 -0800

    Revert "Alt approach"
    This reverts commit 8095cb680d5347d8c7598f41cb3518a70f9c9fba.

--- src/vendor/azure-uamqp-c/src/message_sender.c
+++ src/vendor/azure-uamqp-c/src/message_sender.c
@@ -144,6 +144,8 @@ static void on_delivery_settled(void* context, delivery_number delivery_no, LINK
             message_with_callback->on_message_send_complete(message_with_callback->context, MESSAGE_SEND_TIMEOUT, NULL);
             break;
         case LINK_DELIVERY_SETTLE_REASON_NOT_DELIVERED:
+            LogError("Message not delivered.");
+            break;
         default:
             message_with_callback->on_message_send_complete(message_with_callback->context, MESSAGE_SEND_ERROR, NULL);
             break;
