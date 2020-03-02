eb4889bc0af04dc417601c21edaff261f57cc9ee
Brandon Siegel <brandon.siegel@microsoft.com>
2020-03-02 14:37:24 -0800

    Add scripts to build the ManyLinux docker images

---
+++ utils/ManyLinux2010_64bit/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+[ -z "$TAG" ] && echo "Must set \$TAG to a value (e.g. '3.8')" && exit 1
+
+az acr login --subscription "Azure SDK Engineering System" --name azuresdkimages || exit 1
+az acr build --subscription "Azure SDK Engineering System" -r azuresdkimages -t azuresdkimages.azurecr.io/manylinux2010_crypto_x64:$TAG .
+docker pull azuresdkimages.azurecr.io/manylinux2010_crypto_x64:$TAG
+docker tag azuresdkimages.azurecr.io/manylinux2010_crypto_x64:$TAG azuresdkimages.azurecr.io/manylinux2010_crypto_x64:latest
+docker push azuresdkimages.azurecr.io/manylinux2010_crypto_x64:latest
