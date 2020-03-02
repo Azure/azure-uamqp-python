810ba8d14f637d5dbcd8dcf1743a396fca9c9b26
bsiegel <96068+bsiegel@users.noreply.github.com>
2020-03-02 14:46:30 -0800

    Build manylinux2010 wheels (#128)
    * Add ManyLinux2010 Dockerfile
    
    * Build manylinux2010 wheels
    
    * Parallelize the manylinux build
    
    * Add scripts to build the ManyLinux docker images

--- build_many_linux.sh
+++ build_many_linux.sh
@@ -3,26 +3,27 @@ set -e
 
 # Inspired by https://github.com/pypa/python-manylinux-demo/blob/a615d78e5042c01a03e1bbb1ca78603c90dbce1f/travis/build-wheels.sh
 
-# To build 64bit wheels, run:
-# docker run --rm -v $PWD:/data -e "UAMQP_REBUILD_PYX=True" local/manylinux_crypto64 /data/build_many_linux.sh
+# To build a 64bit manylinux1 wheel (for e.g. Python 3.8), run:
+# docker run --rm -v $PWD:/data -e "PYTHON_VERSION=cp38-cp38" -e "PLATFORM=manylinux1_x86_64" -e "UAMQP_REBUILD_PYX=True" azuresdkimages.azurecr.io/manylinux_crypto_x64 /data/build_many_linux.sh
+
+# To build a 64bit manylinux2010 wheel (for e.g. Python 3.8), run:
+# docker run --rm -v $PWD:/data -e "PYTHON_VERSION=cp38-cp38" -e "PLATFORM=manylinux2010_x86_64" -e "UAMQP_REBUILD_PYX=True" azuresdkimages.azurecr.io/manylinux2010_crypto_x64 /data/build_many_linux.sh
 
 export CPATH="/opt/pyca/cryptography/openssl/include"
 export LIBRARY_PATH="/opt/pyca/cryptography/openssl/lib"
 export OPENSSL_ROOT_DIR="/opt/pyca/cryptography/openssl"
+export PYBIN="/opt/python/$PYTHON_VERSION/bin"
 
-# Build the wheels
+# Build the wheel
 pushd /data;
-for PYBIN in /opt/python/*/bin; do
-        if [[ "$PYBIN" =~ "cp34" ]]; then continue; fi; # Skip Python 3.4
-        $PYBIN/pip install cython==0.28.5 wheel;
-        $PYBIN/python setup.py bdist_wheel -d /wheelhouse;
-        rm -rf build/
-done;
+$PYBIN/pip install cython==0.28.5 wheel;
+$PYBIN/python setup.py bdist_wheel -d /wheelhouse;
+rm -rf build/;
 popd;
 
-# Repair the wheels
+# Repair the wheel
 for WHL in /wheelhouse/*; do
-        auditwheel repair $WHL -w /data/wheelhouse/;
+        auditwheel repair --plat $PLATFORM $WHL -w /data/wheelhouse/;
 done;
 
 # Set up env vars to run live tests - otherwise they will be skipped.
@@ -37,13 +38,10 @@ export IOTHUB_ENDPOINT=""
 export IOTHUB_SAS_POLICY=""
 export IOTHUB_SAS_KEY=""
 
-# Test the wheels
-for PYBIN in /opt/python/*/bin; do
-        if [[ "$PYBIN" =~ "cp34" ]]; then continue; fi; # Skip Python 3.4
-        $PYBIN/pip install "certifi>=2017.4.17" "six~=1.0" "enum34>=1.0.4" "pytest" "pylint";
-        $PYBIN/pip install uamqp --no-index -f /data/wheelhouse;
-        $PYBIN/python -c 'import uamqp;print("*****Importing uamqp from wheel successful*****")';
-        pushd /data;
-        $PYBIN/pytest -v;
-        popd;
-done
+# Test the wheel
+$PYBIN/pip install "certifi>=2017.4.17" "six~=1.0" "enum34>=1.0.4" "pytest" "pylint";
+$PYBIN/pip install uamqp --no-index -f /data/wheelhouse;
+$PYBIN/python -c 'import uamqp;print("*****Importing uamqp from wheel successful*****")';
+pushd /data;
+$PYBIN/pytest -v;
+popd;
