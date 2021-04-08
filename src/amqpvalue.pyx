4cf57bc3b31c9e3c7eede8fc426c7b01322a4f5a
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-04-08 09:51:34 -0700

    fix mem leak in amqp session attach

--- src/amqpvalue.pyx
+++ src/amqpvalue.pyx
@@ -891,7 +891,7 @@ cdef class CompositeValue(AMQPValue):
         if index >= self.size:
             raise IndexError("Index is out of range.")
         cdef c_amqpvalue.AMQP_VALUE value
-        value = c_amqpvalue.amqpvalue_get_composite_item_in_place(self._c_value, index)
+        value = c_amqpvalue.amqpvalue_get_composite_item(self._c_value, index)
         if <void*>value == NULL:
             self._value_error()
         try:
