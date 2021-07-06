057d1c4ca48d771621b58196131d992f5c840927
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-07-07 05:53:42 +0800

    [Python AMQP] Fix encode test (#255)
    * fix encode test as now the data is taking array of bytes
    
    * copy anyio requirement
    
    * fix urlparse
    
    * fix quotes
    
    * fix file encoding issue
    
    * more fi fix
    
    * all fi fixes
    
    * fix typos

--- uamqp/message.py
+++ uamqp/message.py
@@ -34,29 +34,29 @@ if _CAN_ADD_DOCSTRING:
 
     The header section carries standard delivery details about the transfer of a Message through the AMQP
     network. If the header section is omitted the receiver MUST assume the appropriate default values for
-    the ﬁelds within the header unless other target or node speciﬁc defaults have otherwise been set.
+    the fields within the header unless other target or node specific defaults have otherwise been set.
 
     :param bool durable: Specify durability requirements.
         Durable Messages MUST NOT be lost even if an intermediary is unexpectedly terminated and restarted.
-        A target which is not capable of fulﬁlling this guarantee MUST NOT accept messages where the durable
+        A target which is not capable of fulfilling this guarantee MUST NOT accept messages where the durable
         header is set to true: if the source allows the rejected outcome then the message should be rejected
         with the precondition-failed error, otherwise the link must be detached by the receiver with the same error.
     :param int priority: Relative Message priority.
-        This ﬁeld contains the relative Message priority. Higher numbers indicate higher priority Messages.
+        This field contains the relative Message priority. Higher numbers indicate higher priority Messages.
         Messages with higher priorities MAY be delivered before those with lower priorities. An AMQP intermediary
         implementing distinct priority levels MUST do so in the following manner:
         
-            - If n distinct priorities are implemented and n is less than 10 – priorities 0 to (5 - ceiling(n/2))
-              MUST be treated equivalently and MUST be the lowest effective priority. The priorities (4 + ﬂoor(n/2))
+            - If n distince priorities are implemented and n is less than 10 - priorities 0 to (5 - ceiling(n/2))
+              MUST be treated equivalently and MUST be the lowest effective priority. The priorities (4 + fioor(n/2))
               and above MUST be treated equivalently and MUST be the highest effective priority. The priorities
-              (5 ceiling(n/2)) to (4 + ﬂoor(n/2)) inclusive MUST be treated as distinct priorities.
-            - If n distinct priorities are implemented and n is 10 or greater – priorities 0 to (n - 1) MUST be
+              (5 ceiling(n/2)) to (4 + fioor(n/2)) inclusive MUST be treated as distinct priorities.
+            - If n distinct priorities are implemented and n is 10 or greater - priorities 0 to (n - 1) MUST be
               distinct, and priorities n and above MUST be equivalent to priority (n - 1). Thus, for example, if 2
               distinct priorities are implemented, then levels 0 to 4 are equivalent, and levels 5 to 9 are equivalent
               and levels 4 and 5 are distinct. If 3 distinct priorities are implements the 0 to 3 are equivalent,
               5 to 9 are equivalent and 3, 4 and 5 are distinct. This scheme ensures that if two priorities are distinct
               for a server which implements m separate priority levels they are also distinct for a server which
-              implements n diﬀerent priority levels where n > m.
+              implements n different priority levels where n > m.
 
     :param int ttl: Time to live in ms.
         Duration in milliseconds for which the Message should be considered 'live'. If this is set then a message
@@ -69,8 +69,8 @@ if _CAN_ADD_DOCSTRING:
         If this value is false, then this message may have previously been acquired by another Link or Links.
     :param int delivery_count: The number of prior unsuccessful delivery attempts.
         The number of unsuccessful previous attempts to deliver this message. If this value is non-zero it may
-        be taken as an indication that the delivery may be a duplicate. On ﬁrst delivery, the value is zero.
-        It is incremented upon an outcome being settled at the sender, according to rules deﬁned for each outcome.
+        be taken as an indication that the delivery may be a duplicate. On first delivery, the value is zero.
+        It is incremented upon an outcome being settled at the sender, according to rules defined for each outcome.
     """
 
 
@@ -111,12 +111,12 @@ if _CAN_ADD_DOCSTRING:
     Properties.__doc__ = """
     Immutable properties of the Message.
 
-    The properties section is used for a deﬁned set of standard properties of the message. The properties
+    The properties section is used for a defined set of standard properties of the message. The properties
     section is part of the bare message and thus must, if retransmitted by an intermediary, remain completely
     unaltered.
 
-    :param message_id: Application Message identiﬁer.
-        Message-id is an optional property which uniquely identiﬁes a Message within the Message system.
+    :param message_id: Application Message identifier.
+        Message-id is an optional property which uniquely identifies a Message within the Message system.
         The Message producer is usually responsible for setting the message-id in such a way that it is assured
         to be globally unique. A broker MAY discard a Message as a duplicate if the value of the message-id
         matches that of a previously received Message sent to the same Node.
@@ -124,24 +124,24 @@ if _CAN_ADD_DOCSTRING:
         The identity of the user responsible for producing the Message. The client sets this value, and it MAY
         be authenticated by intermediaries.
     :param to: The address of the Node the Message is destined for.
-        The to ﬁeld identiﬁes the Node that is the intended destination of the Message. On any given transfer
+        The to field identifies the Node that is the intended destination of the Message. On any given transfer
         this may not be the Node at the receiving end of the Link.
     :param str subject: The subject of the message.
-        A common ﬁeld for summary information about the Message content and purpose.
+        A common field for summary information about the Message content and purpose.
     :param reply_to: The Node to send replies to.
         The address of the Node to send replies to.
-    :param correlation_id: Application correlation identiﬁer.
-        This is a client-speciﬁc id that may be used to mark or identify Messages between clients.
+    :param correlation_id: Application correlation identifier.
+        This is a client-specific id that may be used to mark or identify Messages between clients.
     :param bytes content_type: MIME content type.
         The RFC-2046 MIME type for the Message's application-data section (body). As per RFC-2046 this may contain
-        a charset parameter deﬁning the character encoding used: e.g. 'text/plain; charset="utf-8"'.
+        a charset parameter defining the character encoding used: e.g. 'text/plain; charset="utf-8"'.
         For clarity, the correct MIME type for a truly opaque binary section is application/octet-stream.
         When using an application-data section with a section code other than data, contenttype, if set, SHOULD
         be set to a MIME type of message/x-amqp+?, where '?' is either data, map or list.
     :param bytes content_encoding: MIME content type.
-        The Content-Encoding property is used as a modiﬁer to the content-type. When present, its value indicates
+        The Content-Encoding property is used as a modifier to the content-type. When present, its value indicates
         what additional content encodings have been applied to the application-data, and thus what decoding
-        mechanisms must be applied in order to obtain the media-type referenced by the content-type header ﬁeld.
+        mechanisms must be applied in order to obtain the media-type referenced by the content-type header field.
         Content-Encoding is primarily used to allow a document to be compressed without losing the identity of
         its underlying content type. Content Encodings are to be interpreted as per Section 3.5 of RFC 2616.
         Valid Content Encodings are registered at IANA as "Hypertext Transfer Protocol (HTTP) Parameters"
@@ -156,11 +156,11 @@ if _CAN_ADD_DOCSTRING:
     :param datetime creation_time: The time when this message was created.
         An absolute time when this message was created.
     :param str group_id: The group this message belongs to.
-        Identiﬁes the group the message belongs to.
+        Identifies the group the message belongs to.
     :param int group_sequence: The sequence-no of this message within its group.
         The relative position of this message within its group.
     :param str reply_to_group_id: The group the reply message belongs to.
-        This is a client-speciﬁc id that is used so that client can send replies to this message to a speciﬁc group.
+        This is a client-specific id that is used so that client can send replies to this message to a specific group.
     """
 
 # TODO: should be a class, namedtuple or dataclass, immutability vs performance, need to collect performance data
@@ -194,9 +194,9 @@ if _CAN_ADD_DOCSTRING:
     An annotated message consists of the bare message plus sections for annotation at the head and tail
     of the bare message.
 
-    There are two classes of annotations: annotations that travel with the message indeﬁnitely, and
+    There are two classes of annotations: annotations that travel with the message indefinitely, and
     annotations that are consumed by the next node.
-    The exact structure of a message, together with its encoding, is deﬁned by the message format. This document
+    The exact structure of a message, together with its encoding, is defined by the message format. This document
     defines the structure and semantics of message format 0 (MESSAGE-FORMAT). Altogether a message consists of the
     following sections:
 
@@ -212,34 +212,34 @@ if _CAN_ADD_DOCSTRING:
     :param ~uamqp.message.Header header: Transport headers for a Message.
         The header section carries standard delivery details about the transfer of a Message through the AMQP
         network. If the header section is omitted the receiver MUST assume the appropriate default values for
-        the ﬁelds within the header unless other target or node speciﬁc defaults have otherwise been set.
-    :param dict delivery_annotations: The delivery-annotations section is used for delivery-speciﬁc non-standard
+        the fields within the header unless other target or node specific defaults have otherwise been set.
+    :param dict delivery_annotations: The delivery-annotations section is used for delivery-specific non-standard
         properties at the head of the message. Delivery annotations convey information from the sending peer to
         the receiving peer. If the recipient does not understand the annotation it cannot be acted upon and its
-        effects (such as any implied propagation) cannot be acted upon. Annotations may be speciﬁc to one
+        effects (such as any implied propagation) cannot be acted upon. Annotations may be specific to one
         implementation, or common to multiple implementations. The capabilities negotiated on link attach and on
-        the source and target should be used to establish which annotations a peer supports. A registry of deﬁned
+        the source and target should be used to establish which annotations a peer supports. A registry of defined
         annotations and their meanings can be found here: http://www.amqp.org/specification/1.0/delivery-annotations.
         If the delivery-annotations section is omitted, it is equivalent to a delivery-annotations section
         containing an empty map of annotations.
     :param dict message_annotations: The message-annotations section is used for properties of the message which
         are aimed at the infrastructure and should be propagated across every delivery step. Message annotations
         convey information about the message. Intermediaries MUST propagate the annotations unless the annotations
-        are explicitly augmented or modiﬁed (e.g. by the use of the modified outcome).
+        are explicitly augmented or modified (e.g. by the use of the modified outcome).
         The capabilities negotiated on link attach and on the source and target may be used to establish which
         annotations a peer understands, however it a network of AMQP intermediaries it may not be possible to know
         if every intermediary will understand the annotation. Note that for some annotation it may not be necessary
         for the intermediary to understand their purpose - they may be being used purely as an attribute which can be
-        ﬁltered on. A registry of deﬁned annotations and their meanings can be found here:
+        filtered on. A registry of defined annotations and their meanings can be found here:
         http://www.amqp.org/specification/1.0/message-annotations. If the message-annotations section is omitted,
         it is equivalent to a message-annotations section containing an empty map of annotations.
     :param ~uamqp.message.Properties: Immutable properties of the Message.
-        The properties section is used for a deﬁned set of standard properties of the message. The properties
+        The properties section is used for a defined set of standard properties of the message. The properties
         section is part of the bare message and thus must, if retransmitted by an intermediary, remain completely
         unaltered.
     :param dict application_properties: The application-properties section is a part of the bare message used
         for structured application data. Intermediaries may use the data within this structure for the purposes
-        of ﬁltering or routing. The keys of this map are restricted to be of type string (which excludes the
+        of filtering or routing. The keys of this map are restricted to be of type string (which excludes the
         possibility of a null key) and the values are restricted to be of simple types only (that is excluding
         map, list, and array types).
     :param list(bytes) data_body: A data section contains opaque binary data.
@@ -248,7 +248,7 @@ if _CAN_ADD_DOCSTRING:
     :param dict footer: Transport footers for a Message.
         The footer section is used for details about the message or delivery which can only be calculated or
         evaluated once the whole bare message has been constructed or seen (for example message hashes, HMACs,
-        signatures and encryption details). A registry of deﬁned footers and their meanings can be found
+        signatures and encryption details). A registry of defined footers and their meanings can be found
         here: http://www.amqp.org/specification/1.0/footer.
     """
 
