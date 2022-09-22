3f5c38eecd976e1cc2f8bb8411abc929414881ff
Kashif Khan <kashifkhan@microsoft.com>
2022-09-22 13:47:11 -0500

    add in support for Linux builds

---
+++ utils/install_openssl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -xe
+
+OPENSSL_URL="https://www.openssl.org/source/"
+
+export OPENSSL_VERSION="openssl-1.1.1n"
+export OPENSSL_SHA256="40dceb51a4f6a5275bde0e6bf20ef4b91bfc32ed57c0552e2e8e15463372b17a"
+# We need a base set of flags because on Windows using MSVC
+# enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
+export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-comp no-dynamic-engine"
+export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"
+
+function check_sha256sum {
+    local fname=$1
+    local sha256=$2
+    echo "${sha256}  ${fname}" > "${fname}.sha256"
+    sha256sum -c "${fname}.sha256"
+    rm "${fname}.sha256"
+}
+
+curl -#O "${OPENSSL_URL}/${OPENSSL_VERSION}.tar.gz"
+check_sha256sum ${OPENSSL_VERSION}.tar.gz ${OPENSSL_SHA256}
+tar zxf ${OPENSSL_VERSION}.tar.gz
+PATH=/opt/perl/bin:$PATH
+pushd ${OPENSSL_VERSION}
+./config $OPENSSL_BUILD_FLAGS --prefix=/opt/pyca/cryptography/openssl --openssldir=/opt/pyca/cryptography/openssl
+make depend
+make -j4
+# avoid installing the docs
+# https://github.com/openssl/openssl/issues/6685#issuecomment-403838728
+make install_sw install_ssldirs
+popd
+rm -rf openssl*
