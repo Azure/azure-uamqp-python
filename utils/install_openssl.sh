d7ddab572a5658a16210fd2dbc240bd9b0bfa026
Kashif Khan <kashifkhan@microsoft.com>
2023-02-07 16:59:08 -0600

    update to openssl 1.1.1t

--- utils/install_openssl.sh
+++ utils/install_openssl.sh
@@ -3,8 +3,8 @@ set -xe
 
 OPENSSL_URL="https://www.openssl.org/source/"
 
-export OPENSSL_VERSION="openssl-1.1.1s"
-export OPENSSL_SHA256="c5ac01e760ee6ff0dab61d6b2bbd30146724d063eb322180c6f18a6f74e4b6aa"
+export OPENSSL_VERSION="openssl-1.1.1t"
+export OPENSSL_SHA256="8dee9b24bdb1dcbf0c3d1e9b02fb8f6bf22165e807f45adeb7c9677536859d3b"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-comp no-dynamic-engine"
